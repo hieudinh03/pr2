fbe1b25781775b6f45f14fe7f30a3cae121c85f1
hieudinh03 <135002575+hieudinh03@users.noreply.github.com>
2024-01-25 23:46:38 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-//thiet ke model
 class Product{
   String search_image;
   String styleid;
@@ -11,22 +9,21 @@ class Product{
   String price;
   String product_additional_info;
 
-  Product(
-      {
-        required this.search_image,
-        required this.styleid,
-        required this.brands_filter_facet,
-        required this.price,
-        required this.product_additional_info
-      }
-  );
+  Product({
+    required this.search_image,
+    required this.styleid,
+    required this.brands_filter_facet,
+    required this.price,
+    required this.product_additional_info});
 }
-// thiet ke activity
-class ProductListScreen extends StatefulWidget {
+class ProductListScreen extends StatefulWidget{
   @override
-  _ProductListScreenState createState() => _ProductListScreenState();
+  _ProductListScreenState createState() =>_ProductListScreenState();
+
+
 }
-class _ProductListScreenState extends State<ProductListScreen> {
+
+class _ProductListScreenState  extends State<ProductListScreen>{
   late List<Product> products;
   @override
   void initState() {
@@ -35,68 +32,70 @@ class _ProductListScreenState extends State<ProductListScreen> {
     products=[];
     fetchProducts();
   }
-  Future<void> fetchProducts() async {
-    final response = await http.get(Uri.parse("http://192.168.88.139:8080/aserver/get.php"));
-    if(response.statusCode==200)
-      {
-        final Map<String , dynamic> data=json.decode(response.body);
+  Future<void> fetchProducts() async{
+    final response= await http.get(Uri.parse("http://133.158.1.105:8080/apr/get.php"));
+    if (response.statusCode==200)
+    {
+        final Map<String,dynamic> data=json.decode(response.body);
         setState(() {
-          products=convertMapToList(data);
+          products= convertMaptoList(data);
         });
-      }
-    else{
-      throw Exception("Khong doc duoc du lieu");
     }
-  }
-  List<Product> convertMapToList(Map<String,dynamic> data)
-  {
-    List<Product> productlist=[];
-    data.forEach((key, value) {
-      for(int i=0;i<value.length;i++){
-        Product product=Product(
-            search_image: value[i]['search_image'] ?? '',
-            styleid: value[i]['styleid'] ?? 0,
-            brands_filter_facet: value[i]['brands_filter_facet'] ?? '',
-            price: value[i]['price'] ?? 0,
-            product_additional_info: value[i]['product_additional_info'] ?? '');
-        productlist.add(product);
+    else
+      {
+        throw  Exception("Can't read data");
       }
-    });
-    return productlist;
   }
-  //tao layout
+
+  List<Product> convertMaptoList(Map<String,dynamic> data){
+    List<Product> productList=[];
+    data.forEach((key, value) {
+      for(int i=0;i<value.length;i++)
+        {
+          Product product=Product(
+              search_image:value[i] ['search_image'] ?? '',
+              styleid: value[i]['styleid'] ?? 0,
+              brands_filter_facet: value[i] ['brands_filter_facet'] ?? '',
+              price: value[i] ['price'] ?? 0,
+              product_additional_info: value[i] ['product_additional_info'] ?? '');
+          productList.add(product);
+        }
+    });
+    return productList;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Danh sach san pham"),
-      ),
-      body: products !=null ?
-      ListView.builder(
-          itemCount: products.length,
-          itemBuilder: (context,index){
-            return ListTile(
-              title: Text(products[index].brands_filter_facet),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Price: ${products[index].price}'),
-                  Text('product_additional_info: ${products[index].product_additional_info}'),
-                ],
-              ),
-              leading: Image.network(
-                products[index].search_image,
-                width: 50,
-                height: 50,
-                fit: BoxFit.cover,
-              ),
-            );
-          })
+        title: Text("Products List"),),
+      body: products != null ?
+       ListView.builder(
+            itemCount: products.length,
+           itemBuilder: (context,index){
+              return ListTile(
+                title: Text(products[index].brands_filter_facet),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Price: ${products[index].price}'),
+                    Text('product_additional_info: ${products[index].product_additional_info}'),
+                  ],
+                ),
+                leading: Image.network(
+                  products[index].search_image,
+                  width: 50,
+                  height: 50,
+                  fit: BoxFit.cover,
+                ),
+              );
+           })
       :Center(
         child: CircularProgressIndicator(),
       ),
     );
   }
+
+
 }
 void main() {
   runApp(const MyApp());
@@ -109,12 +108,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Danh sach san pham',
+      title: 'Products List',
       theme: ThemeData(
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProductListScreen(),
+       home: HomeScreen(), //ProductListScreen(),
     );
+  }
+}
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Trang Chu"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: (){
+            Navigator.push(context,
+            MaterialPageRoute(builder: (context)=> ProductListScreen()),);
+          },
+          child: Text('Go to ProductListScreen ')
+        ), //evlevatebutton
+      ), // center
+    );// scaffold
   }
 }
